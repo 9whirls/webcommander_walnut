9f45f4a46c8454d84466aba55805a865990cd211
Jerry Liu <whirls9@hotmail.com>
2017-05-07 00:31:39 -0700

    fix bug

--- powershell/exec.ps1
+++ powershell/exec.ps1
@@ -1,24 +1,20 @@
-. .\utils.ps1
+. ./utils.ps1
 
 $script = $args[0]
 if (test-path ".\$script") {
   $c = get-content $script -raw
 } else {
   try {
-    $web = new-object net.webclient
-    $c = $web.downloadstring($script)
+    $c = (invoke-webrequest $script).content
   } catch {
-    addToResult "Fail - read $script"
+    addToResult "Fail - find script $script"
     endError
   }
 }
 
 $params = @()
 for ( $i = 1; $i -lt $args.count; $i++ ) {
-  $param = $args[$i]
-  if ($param.gettype().name -eq "String") {
-    $param = [system.web.httputility]::urldecode($param)
-  }
+  $param = [System.Net.WebUtility]::urldecode($args[$i])
   if ($param -notmatch '^-') {
     $param = "'" + $param.replace("'","''") + "'"
   }
@@ -33,4 +29,5 @@ try {
   addToResult "Fail - run Powershell script $script"
   addError
 }
+
 writeResult -verbose 4>&1
